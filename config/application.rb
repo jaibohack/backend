b45bf8b7318e96f46577d96fff4c274be7107b39
Daniel Montoya <dsmontoyam@gmail.com>
2015-07-13 06:00:29 +0000

    Allowing CORS requests. Please remove wildcard.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module JaibohackBackend
 
     config.paths.add "app/api", glob: "**/*.rb"
     config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options, :patch]
+      end
+    end
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
