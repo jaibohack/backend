e8f96c9756cfc7808af422d02af23f93e6eff322
Daniel Montoya <dsmontoyam@gmail.com>
2015-07-14 05:39:50 +0000

    Make name downcase.

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,10 @@
 class School < ActiveRecord::Base
+  before_save :lowercase
   has_many :attendees
 
   validates :name, uniqueness: true
+
+  def lowercase
+    self.name = self.name.downcase
+  end
 end
